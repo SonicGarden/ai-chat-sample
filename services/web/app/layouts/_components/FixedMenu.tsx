4a6d4f164125f20a48e5ae1771920ae6423afd13
hiroro-work <hiroro@sonicgarden.jp>
2024-04-20 06:30:12 +0900

    新しいスレッドを作成できるように

--- services/web/app/layouts/_components/FixedMenu.tsx
+++ services/web/app/layouts/_components/FixedMenu.tsx
@@ -1,3 +1,22 @@
+import { Button, Stack } from '@mantine/core';
+import { useSearchParams } from '@remix-run/react';
+import { IconPlus } from '@tabler/icons-react';
+import { useCallback } from 'react';
+import { useDefaultLayout } from '../DefaultLayout';
+
 export const FixedMenu = () => {
-  return <div>固定メニュー</div>;
+  const [, setSearchParams] = useSearchParams();
+  const { navbar } = useDefaultLayout();
+  const handleClickButton = useCallback(() => {
+    setSearchParams({});
+    navbar.toggle();
+  }, [navbar, setSearchParams]);
+
+  return (
+    <Stack>
+      <Button variant='default' leftSection={<IconPlus size={16} />} onClick={handleClickButton}>
+        New Chat
+      </Button>
+    </Stack>
+  );
 };
