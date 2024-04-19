172fca58e737add85814a5754463f8fa0b8d9204
hiroro-work <hiroro@sonicgarden.jp>
2024-04-19 17:22:16 +0900

    Mantineが動くように

--- services/web/app/root.tsx
+++ services/web/app/root.tsx
@@ -1,18 +1,22 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { MantineProvider, ColorSchemeScript } from '~/utils/mantine/provider';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='ja'>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
+        <ColorSchemeScript />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <MantineProvider>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </MantineProvider>
       </body>
     </html>
   );
