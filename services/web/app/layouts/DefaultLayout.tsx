01140fd6a3bbfdde9c3c8b0dd7c7b5a1af1c0dce
hiroro-work <hiroro@sonicgarden.jp>
2024-04-19 20:48:23 +0900

    未認証の場合はサインインページにリダイレクトするように

--- services/web/app/layouts/DefaultLayout.tsx
+++ services/web/app/layouts/DefaultLayout.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from '@remix-run/react';
+import { useEffect } from 'react';
 import { ResponsiveLayout, useResponsiveLayoutContext } from '~/components/layouts/ResponsiveLayout';
+import { LoadingScreen } from '~/components/screens/LoadingScreen';
+import { useAuth } from '~/hooks/firebase/useAuth';
 import { AccountMenu } from './_components/AccountMenu';
 import { FixedMenu } from './_components/FixedMenu';
 import { HeaderTitle } from './_components/HeaderTitle';
@@ -6,6 +10,15 @@ import { NavMenu } from './_components/NavMenu';
 import type { ReactNode } from 'react';
 
 export const DefaultLayout = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate();
+  const { signedIn } = useAuth();
+
+  useEffect(() => {
+    if (signedIn === false) navigate('/sign-in');
+  }, [signedIn, navigate]);
+
+  if (!signedIn) return <LoadingScreen />;
+
   return (
     <ResponsiveLayout
       header={{ title: <HeaderTitle /> }}
