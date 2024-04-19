7d0f3b43b164b85848f6c249dda1e04040a076a5
hiroro-work <hiroro@sonicgarden.jp>
2024-04-19 21:06:42 +0900

    アカウントメニューからサインアウトできるように

--- services/web/app/layouts/_components/AccountMenu.tsx
+++ services/web/app/layouts/_components/AccountMenu.tsx
@@ -1,3 +1,46 @@
+import { Avatar, Box, Group, NavLink, Text } from '@mantine/core';
+import { useNavigate } from '@remix-run/react';
+import { useCallback } from 'react';
+import { UnstyledConfirmButton } from '~/components/elements/buttons/UnstyledConfirmButton';
+import { useAuth } from '~/hooks/firebase/useAuth';
+import { signOut } from '~/utils/firebase/auth';
+import { notify } from '~/utils/mantine/notifications';
+
 export const AccountMenu = () => {
-  return <div>アカウントメニュー</div>;
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const handleConfirmSignOut = useCallback(async () => {
+    await signOut();
+    notify.info({
+      message: 'サインアウトしました',
+    });
+  }, []);
+
+  return (
+    <Box aria-label='アカウントメニュー'>
+      {user ? (
+        <>
+          <NavLink
+            label={
+              <Group wrap='nowrap'>
+                <Avatar size='sm' />
+                <Text truncate='end'>{user.email}</Text>
+              </Group>
+            }
+          >
+            <NavLink
+              label='サインアウト'
+              component={UnstyledConfirmButton}
+              message='本当にサインアウトしますか？'
+              onConfirm={handleConfirmSignOut}
+            />
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink label='サインイン' onClick={() => navigate('sign-in', { replace: true })}></NavLink>
+        </>
+      )}
+    </Box>
+  );
 };
