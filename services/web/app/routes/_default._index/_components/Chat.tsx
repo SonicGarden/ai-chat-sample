a97be7d0e7cb527f7c34615a6d04a5ac1c7d1ce3
hiroro-work <hiroro@sonicgarden.jp>
2024-04-20 06:11:34 +0900

    チャット画面でチャットデータを取得するように

--- services/web/app/routes/_default._index/_components/Chat.tsx
+++ services/web/app/routes/_default._index/_components/Chat.tsx
@@ -1,5 +1,9 @@
 import { Box, Stack } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
+import { useSearchParams } from '@remix-run/react';
+import { useMemo } from 'react';
+import { useDocumentData } from '~/hooks/firebase/useDocumentData';
+import { threadContentRef as _threadContentRef } from '~/models/threadContent';
 import { ChatForm } from './ChatForm';
 import { ChatMessage } from './ChatMessage';
 import classes from './_styles/Chat.module.css';
@@ -7,8 +11,12 @@ import classes from './_styles/Chat.module.css';
 const FORM_MARGIN = 32;
 
 export const Chat = ({ height }: { height: number }) => {
+  const [searchParams] = useSearchParams();
+  const threadId = searchParams.get('threadId');
   const { ref: formRef, height: formHeight } = useElementSize();
   const scrollAreaHeight = height - formHeight - FORM_MARGIN * 2;
+  const threadContentRef = useMemo(() => _threadContentRef({ id: threadId }), [threadId]);
+  const { data: threadContent } = useDocumentData(threadContentRef);
 
   return (
     <Stack gap={0} justify='space-between'>
@@ -25,6 +33,7 @@ export const Chat = ({ height }: { height: number }) => {
             }}
           />
         ))}
+        {threadContent?.messages.map((message, index) => <ChatMessage key={index} message={message} />)}
       </Box>
       <ChatForm m={FORM_MARGIN} ref={formRef} />
     </Stack>
