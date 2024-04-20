50cd8688bd58a1939404ccf4a9a5c099ae3a3409
hiroro-work <hiroro@sonicgarden.jp>
2024-04-20 15:36:47 +0900

    スレッド履歴を無限スクロールに

--- services/web/app/components/elements/Loader.tsx
+++ services/web/app/components/elements/Loader.tsx
@@ -1,6 +1,8 @@
 import { Loader as MantineLoader } from '@mantine/core';
+import { forwardRef } from 'react';
 import type { LoaderProps } from '@mantine/core';
 
-export const Loader = (props: LoaderProps) => {
-  return <MantineLoader color='gray' type='dots' {...props} />;
-};
+export const Loader = forwardRef<HTMLSpanElement, LoaderProps>((props: LoaderProps, ref) => {
+  return <MantineLoader color='gray' type='dots' ref={ref} {...props} />;
+});
+Loader.displayName = 'Loader';
--- services/web/app/layouts/_components/NavMenu.tsx
+++ services/web/app/layouts/_components/NavMenu.tsx
@@ -1,29 +1,37 @@
 import { Stack, NavLink } from '@mantine/core';
 import { useSearchParams } from '@remix-run/react';
 import Markdown from 'react-markdown';
+import { InfiniteScroll } from '~/components/elements/InfiniteScroll';
 import { useAuth } from '~/hooks/firebase/useAuth';
-import { useCollectionData } from '~/hooks/firebase/useCollectionData';
+import { usePaginatedCollectionData } from '~/hooks/firebase/usePaginatedCollectionData';
 import { threadByUidQuery } from '~/models/thread';
 import classes from './_styles/NavMenu.module.css';
 
 export const NavMenu = () => {
   const [, setSearchParams] = useSearchParams();
   const { user } = useAuth();
-  const { data: threads } = useCollectionData(threadByUidQuery({ uid: user!.uid }));
+  const {
+    data: threads,
+    loading,
+    hasMore,
+    loadMore,
+  } = usePaginatedCollectionData(threadByUidQuery({ uid: user!.uid }), { limit: 20 });
 
   return (
     <Stack>
-      {threads?.map((thread) => (
-        <NavLink
-          key={thread.id}
-          label={
-            <Markdown components={{ p: ({ children }) => <p className={classes.label}>{children}</p> }}>
-              {thread.title}
-            </Markdown>
-          }
-          onClick={() => setSearchParams({ threadId: thread.id })}
-        />
-      ))}
+      <InfiniteScroll loading={loading} hasMore={hasMore} loadMore={loadMore} loaderProps={{ size: 'sm', mx: 'auto' }}>
+        {threads?.map((thread) => (
+          <NavLink
+            key={thread.id}
+            label={
+              <Markdown components={{ p: ({ children }) => <p className={classes.label}>{children}</p> }}>
+                {thread.title}
+              </Markdown>
+            }
+            onClick={() => setSearchParams({ threadId: thread.id })}
+          />
+        ))}
+      </InfiniteScroll>
     </Stack>
   );
 };
